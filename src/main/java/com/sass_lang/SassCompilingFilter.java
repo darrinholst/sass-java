70fb9fc685034dbf1223c583a267193d8b907a8f
Darrin Holst <darrinholst@gmail.com>
2011-05-06 14:43:58 -0500

    build the script at init time

--- src/main/java/com/sass_lang/SassCompilingFilter.java
+++ src/main/java/com/sass_lang/SassCompilingFilter.java
@@ -14,16 +14,16 @@ public class SassCompilingFilter implements Filter {
     protected static final String DEFAULT_CSS_LOCATION = "stylesheets";
     protected static final String DEFAULT_CACHE_LOCATION = "WEB-INF" + File.separator + ".sass-cache";
 
-    private String templateLocation;
-    private String cssLocation;
-    private String cacheLocation;
+    private String checkForUpdatesScript;
     private long lastRun;
 
     public void init(FilterConfig filterConfig) throws ServletException {
         String root = new File(filterConfig.getServletContext().getRealPath("/")).getAbsolutePath();
-        templateLocation = fullPath(root, filterConfig.getInitParameter("templateLocation"), DEFAULT_TEMPLATE_LOCATION);
-        cssLocation = fullPath(root, filterConfig.getInitParameter("cssLocation"), DEFAULT_CSS_LOCATION);
-        cacheLocation = fullPath(root, filterConfig.getInitParameter("cacheLocation"), DEFAULT_CACHE_LOCATION);
+        String templateLocation = fullPath(root, filterConfig.getInitParameter("templateLocation"), DEFAULT_TEMPLATE_LOCATION);
+        String cssLocation = fullPath(root, filterConfig.getInitParameter("cssLocation"), DEFAULT_CSS_LOCATION);
+        String cacheLocation = fullPath(root, filterConfig.getInitParameter("cacheLocation"), DEFAULT_CACHE_LOCATION);
+        
+        checkForUpdatesScript = checkForUpdatesScript(templateLocation, cssLocation, cacheLocation);
     }
 
     private String fullPath(String root, String directory, String defaultDirectory) {
@@ -35,25 +35,29 @@ public class SassCompilingFilter implements Filter {
         
         if (now - lastRun >= DWELL) {
             lastRun = now;
-            StringWriter raw = new StringWriter();
-            PrintWriter script = new PrintWriter(raw);
-
-            script.println("  require 'rubygems'                                  ");
-            script.println("  require 'sass/plugin'                               ");
-            script.println("  Sass::Plugin.options.merge!(                        ");
-            script.println("    :template_location => '" + templateLocation + "', ");
-            script.println("    :css_location => '" + cssLocation + "',           ");
-            script.println("    :cache_store => nil,                              ");
-            script.println("    :cache_location => '" + cacheLocation + "'        ");
-            script.println("  )                                                   ");
-            script.println("  Sass::Plugin.check_for_updates                      ");
-            script.flush();
-
             ScriptingContainer ruby = new ScriptingContainer();
-            ruby.runScriptlet(raw.toString());
+            ruby.runScriptlet(checkForUpdatesScript);
         }
 
         filterChain.doFilter(servletRequest, servletResponse);
+    }
+
+    private String checkForUpdatesScript(String templateLocation, String cssLocation, String cacheLocation) {
+        StringWriter raw = new StringWriter();
+        PrintWriter script = new PrintWriter(raw);
+
+        script.println("  require 'rubygems'                                  ");
+        script.println("  require 'sass/plugin'                               ");
+        script.println("  Sass::Plugin.options.merge!(                        ");
+        script.println("    :template_location => '" + templateLocation + "', ");
+        script.println("    :css_location => '" + cssLocation + "',           ");
+        script.println("    :cache_store => nil,                              ");
+        script.println("    :cache_location => '" + cacheLocation + "'        ");
+        script.println("  )                                                   ");
+        script.println("  Sass::Plugin.check_for_updates                      ");
+        script.flush();
+
+        return raw.toString();
     }
 
     public void destroy() {
