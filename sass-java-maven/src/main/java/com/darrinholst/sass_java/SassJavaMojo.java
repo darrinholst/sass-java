520574cb392546d40e41d53f85352a530bdf168b
windgazer <martin@windgazer.nl>
2014-09-30 16:25:44 +0200

    Hello world!

--- sass-java-maven/src/main/java/com/darrinholst/sass_java/SassJavaMojo.java
+++ sass-java-maven/src/main/java/com/darrinholst/sass_java/SassJavaMojo.java
@@ -3,15 +3,14 @@ package com.darrinholst.sass_java;
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.MojoFailureException;
-import org.apache.maven.plugins.annotations.LifecyclePhase;
 import org.apache.maven.plugins.annotations.Mojo;
 
 @Mojo(
-	name = "compile",
-	defaultPhase = LifecyclePhase.COMPILE
+	name = "sayhi"
 )
 public class SassJavaMojo extends AbstractMojo {
 
+	
 	public void execute() throws MojoExecutionException, MojoFailureException {
 		// TODO Auto-generated method stub
 		getLog().info( "Hello, world." );
