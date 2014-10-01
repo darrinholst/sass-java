6ae3e3958f3915de2cf10c83e4f4457dd94a2a6b
Darrin Holst <darrinholst@gmail.com>
2014-10-01 13:02:17 -0500

    formatting
    Closes #12

--- sass-java-maven/src/main/java/com/darrinholst/sass_java/SassJavaMojo.java
+++ sass-java-maven/src/main/java/com/darrinholst/sass_java/SassJavaMojo.java
@@ -1,7 +1,5 @@
 package com.darrinholst.sass_java;
 
-import java.io.File;
-
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.MojoFailureException;
@@ -9,37 +7,24 @@ import org.apache.maven.plugins.annotations.LifecyclePhase;
 import org.apache.maven.plugins.annotations.Mojo;
 import org.apache.maven.plugins.annotations.Parameter;
 
-@Mojo(
-	name = "compile",
-	defaultPhase = LifecyclePhase.COMPILE,
-	requiresDirectInvocation = false
-)
+import java.io.File;
+
+@Mojo(name = "compile", defaultPhase = LifecyclePhase.COMPILE, requiresDirectInvocation = false)
 public class SassJavaMojo extends AbstractMojo {
+    private Compiler compiler = new Compiler();
 
-	private Compiler compiler = new Compiler();
-
-    @Parameter(
-		defaultValue = "${project.basedir}",
-		readonly = true
-	)
+    @Parameter(defaultValue = "${project.basedir}", readonly = true)
     private File basedir;
 
-    @Parameter( 
-		defaultValue = "${project.basedir}/src/main/webapp/WEB-INF/sass/config.rb",
-		property = "sass-java.configFile",
-		readonly = false
-	)
+    @Parameter(defaultValue = "${project.basedir}/src/main/webapp/WEB-INF/sass/config.rb", property = "sass-java.configFile")
     private File config;
 
-	public void execute() throws MojoExecutionException, MojoFailureException {
-
-		if (config.exists()) {
-	        compiler.setConfigLocation( config );
-	        compiler.compile();
-		} else {
-			throw new MojoExecutionException( "Configuration does not exist at " + config.getAbsolutePath() );
-		}
-
-	}
-
+    public void execute() throws MojoExecutionException, MojoFailureException {
+        if (config.exists()) {
+            compiler.setConfigLocation(config);
+            compiler.compile();
+        } else {
+            throw new MojoExecutionException("Configuration does not exist at " + config.getAbsolutePath());
+        }
+    }
 }
