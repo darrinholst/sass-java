13000b88bc5a67ef91d30ecbdcc79fab766242f2
windgazer <martin@windgazer.nl>
2014-10-01 12:43:27 +0200

    Allowing mojo to run during `compile` phase: ``` <plugin> 	<groupId>com.darrinholst</groupId> 	<artifactId>sass-java-maven-plugin</artifactId>     			<version>3.4.4.1-SNAPSHOT</version> 	<executions> 		<execution> 			<goals> 				<goal>compile</goal> 			</goals> 		</execution> 	</executions> </plugin> ```

--- sass-java-maven/src/main/java/com/darrinholst/sass_java/SassJavaMojo.java
+++ sass-java-maven/src/main/java/com/darrinholst/sass_java/SassJavaMojo.java
@@ -5,11 +5,14 @@ import java.io.File;
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.MojoFailureException;
+import org.apache.maven.plugins.annotations.LifecyclePhase;
 import org.apache.maven.plugins.annotations.Mojo;
 import org.apache.maven.plugins.annotations.Parameter;
 
 @Mojo(
-	name = "compile"
+	name = "compile",
+	defaultPhase = LifecyclePhase.COMPILE,
+	requiresDirectInvocation = false
 )
 public class SassJavaMojo extends AbstractMojo {
 
@@ -32,7 +35,6 @@ public class SassJavaMojo extends AbstractMojo {
 
 		if (config.exists()) {
 	        compiler.setConfigLocation( config );
-	
 	        compiler.compile();
 		} else {
 			throw new MojoExecutionException( "Configuration does not exist at " + config.getAbsolutePath() );
